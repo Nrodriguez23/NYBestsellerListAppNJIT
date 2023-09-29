6b3b37f469ea140dc1d9fe31e7b039caf27a7696
Nrodriguez23 <144366574+Nrodriguez23@users.noreply.github.com>
2023-09-28 20:47:52 -0400

    commit

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.codepath.bestsellerlistapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codepath.bestsellerlistapp.R.id
+
+/**
+ * The MainActivity for the BestSellerList app.
+ * Launches a [BestSellerBooksFragment].
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(id.content, BestSellerBooksFragment(), null).commit()
+
+
+    }
+}
